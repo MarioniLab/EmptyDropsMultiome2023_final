9f20bc3b760ef202112e7bd6f141d69aba9e7987
stathismegas <stathismegas@gmail.com>
2023-07-12 15:32:46 +0100

    change date to 20230711; gene dotplot for supporting cell

--- real/downstream_doublet_more_qc_rna.R
+++ real/downstream_doublet_more_qc_rna.R
@@ -16,7 +16,7 @@ source("simulations/fcn_for_sim.R")
 # );
 # opt_parser = OptionParser(option_list=option_list);
 # opt = parse_args(opt_parser);
-old_date <-  "20230614"  #"20230517"   #"20230329" #opt$date
+old_date <-  "20230711"  #"20230517"   #"20230329" #opt$date
 samples <- c("valentina_8176", "valentina_8177")  #, "valentina_8177")
 
 for (sample in samples){
@@ -76,7 +76,7 @@ plot( vale$seurat_clusters , xlab="cluster", main="before qc: total cells" )
 # re-preprocess
 vale_clean <- SCTransform(vale_clean)
 vale_clean <- RunPCA(vale_clean, seed.use=42, features = VariableFeatures(object = vale_clean))
-print(ElbowPlot(vale_clean, ndims = 50)     )
+#print(ElbowPlot(vale_clean, ndims = 50)     )
 vale_clean <- FindNeighbors(vale_clean, dims = 1:50)
 vale_clean <- FindClusters(vale_clean, resolution = 1, random.seed = 0)
 vale_clean <- RunUMAP(vale_clean, dims = 1:50, seed.use = 42)
@@ -86,30 +86,30 @@ plot( vale_clean$seurat_clusters , xlab="cluster", main="after qc: total cells" 
 
 
 # subcluster the CoelEpith+Neuronal
-vale_clean <- FindSubCluster(
-  vale_clean,
-  cluster=9,
-  graph.name="SCT_snn",
-  subcluster.name = "sub.cluster",
-  resolution = 0.2,
-  algorithm = 1
-)
+# vale_clean <- FindSubCluster(
+#   vale_clean,
+#   cluster=9,
+#   graph.name="SCT_snn",
+#   subcluster.name = "sub.cluster",
+#   resolution = 0.2,
+#   algorithm = 1
+# )
 
-vale_clean$sub.cluster[vale_clean$sub.cluster=="9_0"] = "9"
-vale_clean$sub.cluster[vale_clean$sub.cluster=="9_1"] = "9"
+# vale_clean$sub.cluster[vale_clean$sub.cluster=="9_0"] = "9"
+# vale_clean$sub.cluster[vale_clean$sub.cluster=="9_1"] = "9"
 
 
 # QC plots
-print(DimPlot(vale_clean, label=T, group.by = "sub.cluster"))
-print(VlnPlot(vale_clean,group.by = "sub.cluster", features="percent.mt")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue") )
-print(VlnPlot(vale_clean,group.by = "sub.cluster", features="percent.ribo")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
-print(VlnPlot(vale_clean,group.by = "sub.cluster", features="nFeature_RNA")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
-print(VlnPlot(vale_clean,group.by = "sub.cluster", features="nCount_RNA")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
-print(VlnPlot(vale_clean,group.by = "sub.cluster", features="nCount_ATAC")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
-print(VlnPlot(vale_clean,group.by = "sub.cluster", features="nFeature_ATAC")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
+print(DimPlot(vale_clean, label=T, group.by = "seurat_clusters"))
+print(VlnPlot(vale_clean,group.by = "seurat_clusters", features="percent.mt")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue") )
+print(VlnPlot(vale_clean,group.by = "seurat_clusters", features="percent.ribo")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
+print(VlnPlot(vale_clean,group.by = "seurat_clusters", features="nFeature_RNA")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
+print(VlnPlot(vale_clean,group.by = "seurat_clusters", features="nCount_RNA")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
+print(VlnPlot(vale_clean,group.by = "seurat_clusters", features="nCount_ATAC")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
+print(VlnPlot(vale_clean,group.by = "seurat_clusters", features="nFeature_ATAC")+ stat_summary(fun.y = median, geom='point', size = 2, colour = "blue"))
 
 # FRiP plot per cluster
-df_FRiP <- data.frame("frip"=vale_clean$FRiP, "cluster"=vale_clean$sub.cluster)
+df_FRiP <- data.frame("frip"=vale_clean$FRiP, "cluster"=vale_clean$seurat_clusters)
 max_frip_of_excluded = max(vale_clean$FRiP[vale_clean$excluded_reason==2]  )
 print(ggplot(df_FRiP, aes(x = frip, y = cluster, height = stat(density))) + 
         geom_density_ridges(stat = "binline", bins = 80, scale = 0.95, draw_baseline = FALSE)+
@@ -117,9 +117,10 @@ print(ggplot(df_FRiP, aes(x = frip, y = cluster, height = stat(density))) +
         geom_vline(xintercept = max_frip_of_excluded))
 
 # gene marker dotplot
+text_size = 11
 genes_PGC_FGC_FGCmitotic_oogoniaSTRA8 = c("DAZL", "IFITM1", "NANOG", "NANOS3", "POU5F1", "DDX4", "MAEL", "ZGLP1", "STRA8")
 genes_oogoniameiotic_preoocyte_oocyte_prespermatogonia = c( "MEIOC", "SYCP1", "FIGLA", "LHX8", "NOBOX", "GDF9", "ZP3", "DCC")
-DotPlot(vale_clean, group.by = sub.cluster, dot.scale=3, features = c(genes_PGC_FGC_FGCmitotic_oogoniaSTRA8, genes_oogoniameiotic_preoocyte_oocyte_prespermatogonia), 
+print(DotPlot(vale_clean, group.by = "seurat_clusters", dot.scale=3, features = c(genes_PGC_FGC_FGCmitotic_oogoniaSTRA8, genes_oogoniameiotic_preoocyte_oocyte_prespermatogonia), 
                          assay = "SCT")+
     theme(
       # Hide panel borders and remove grid lines
@@ -137,8 +138,34 @@ DotPlot(vale_clean, group.by = sub.cluster, dot.scale=3, features = c(genes_PGC_
     ) + 
     ylab("clusters")+
     ggplot2::ggtitle("")+
-    guides(colour = guide_legend(override.aes = list(size=5)))    
+    guides(colour = guide_legend(override.aes = list(size=5)))  
+      )
+
+genes_germcells_to_mesGATA4 = c( "DAZL", "UPK3B", "GATA4", "LHX9", "NR5A1", "WNT6", "IRX3", "FOXL2", "ARX")
+genes_mesGATA2_to_neural = c( "TCF21", "PDGFRA", "DCN", "GATA2", "NR2F1", "PDGFRB")
+genes_mesGATA2_to_neural2 = c( "MYH11", "PTPRC", "CDH5", "PAX8", "EPCAM", "HBA1")
     
+print(DotPlot(vale_clean, group.by = "seurat_clusters", dot.scale=3, features = c(genes_germcells_to_mesGATA4, genes_mesGATA2_to_neural, genes_mesGATA2_to_neural2), 
+                         assay = "SCT")+
+    theme(
+      # Hide panel borders and remove grid lines
+      panel.border = element_blank(),
+      panel.background = element_rect("white"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.text=element_text(size=text_size),
+      legend.title = element_text(size=text_size),
+      # Change axis line
+      axis.line = element_line(colour = "black"),
+      axis.text=element_text(size=text_size),
+      axis.title=element_text(size=text_size),
+      axis.text.x = element_text(size=text_size, angle=45, vjust=.8, hjust=0.8)
+    ) + 
+    ylab("clusters")+
+    ggplot2::ggtitle("")+
+    guides(colour = guide_legend(override.aes = list(size=5)))  
+      )
+
 
 # UMAP with eD vs rna
 print(DimPlot(vale_clean, group.by = "comparison", sizes.highlight=0.1, reduction = "umap", label=T, cols = c("grey", "blue", "red")) )  
@@ -146,8 +173,8 @@ print(DimPlot(vale_clean, group.by = "comparison", sizes.highlight=0.1, reductio
 
 
 # plot venn diagram eD vs rna
-listInput <- list(eD = colnames(vale_clean)[vale_clean$comparison %in% c("Emptydrops-multiome", "both")], 
-                  eDrna = colnames(vale_clean)[vale_clean$comparison %in% c("eD-rna"  , "both")]
+listInput <- list(eD = colnames(vale_clean)[vale_clean$comparison %in% c("EmptyDropsMultiome", "both")], 
+                  eDrna = colnames(vale_clean)[vale_clean$comparison %in% c("EmptyDrops"  , "both")]
                   )
 overlaps <- euler(listInput, shape = "ellipse")
 p2 <- plot(overlaps, 
@@ -173,29 +200,29 @@ print(DoHeatmap(vale_clean, features = eD_all.markers$gene, size=4,
                 angle = 90) + NoLegend()+ theme(axis.text.y = element_text(size = 5))   )
 
 # calculate rna/eD venn diagram by cluster
-eD_cells = colnames(vale_clean)[vale_clean$comparison %in% c("Emptydrops-multiome", "both")]
-rna_cells = colnames(vale_clean)[vale_clean$comparison %in% c("eD-rna"  , "both")]
-venn_df <- data.frame("0" = c(as.integer(sum(vale_clean$sub.cluster=="0")),
-                              as.integer(sum(vale_clean$sub.cluster=="0" & colnames(vale_clean) %in% eD_cells)),
-                              as.integer(sum(vale_clean$sub.cluster=="0" & colnames(vale_clean) %in% rna_cells)),
-                              sum(vale_clean$sub.cluster=="0" & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells),
-                              sum(vale_clean$sub.cluster=="0" & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$sub.cluster=="0"),
-                              sum(vale_clean$sub.cluster=="0" & colnames(vale_clean) %in% rna_cells)/sum(vale_clean$sub.cluster=="0"),
-                              sum(vale_clean$sub.cluster=="0" & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$sub.cluster=="0")
+eD_cells = colnames(vale_clean)[vale_clean$comparison %in% c("EmptyDropsMultiome", "both")]
+rna_cells = colnames(vale_clean)[vale_clean$comparison %in% c("EmptyDrops"  , "both")]
+venn_df <- data.frame("0" = c(as.integer(sum(vale_clean$seurat_clusters=="0")),
+                              as.integer(sum(vale_clean$seurat_clusters=="0" & colnames(vale_clean) %in% eD_cells)),
+                              as.integer(sum(vale_clean$seurat_clusters=="0" & colnames(vale_clean) %in% rna_cells)),
+                              sum(vale_clean$seurat_clusters=="0" & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells),
+                              sum(vale_clean$seurat_clusters=="0" & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$seurat_clusters=="0"),
+                              sum(vale_clean$seurat_clusters=="0" & colnames(vale_clean) %in% rna_cells)/sum(vale_clean$seurat_clusters=="0"),
+                              sum(vale_clean$seurat_clusters=="0" & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$seurat_clusters=="0")
 ))
-for ( cl in c(1:(max(as.integer(vale_clean$sub.cluster))-1))   ){
+for ( cl in c(1:(max(as.integer(vale_clean$seurat_clusters))-1))   ){
   char_cl = as.character(cl)
-  temp_df <- data.frame("new" = c(sum(vale_clean$sub.cluster==char_cl),
-                                  sum(vale_clean$sub.cluster==char_cl & colnames(vale_clean) %in% eD_cells),
-                                  sum(vale_clean$sub.cluster==char_cl & colnames(vale_clean) %in% rna_cells),
-                                  sum(vale_clean$sub.cluster==char_cl & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells),
-                                  sum(vale_clean$sub.cluster==char_cl & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$sub.cluster==char_cl),
-                                  sum(vale_clean$sub.cluster==char_cl & colnames(vale_clean) %in% rna_cells)/sum(vale_clean$sub.cluster==char_cl),
-                                  sum(vale_clean$sub.cluster==char_cl & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$sub.cluster==char_cl)
+  temp_df <- data.frame("new" = c(sum(vale_clean$seurat_clusters==char_cl),
+                                  sum(vale_clean$seurat_clusters==char_cl & colnames(vale_clean) %in% eD_cells),
+                                  sum(vale_clean$seurat_clusters==char_cl & colnames(vale_clean) %in% rna_cells),
+                                  sum(vale_clean$seurat_clusters==char_cl & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells),
+                                  round(sum(vale_clean$seurat_clusters==char_cl & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$seurat_clusters==char_cl),1),
+                                  round(sum(vale_clean$seurat_clusters==char_cl & colnames(vale_clean) %in% rna_cells)/sum(vale_clean$seurat_clusters==char_cl),1),
+                                  round(sum(vale_clean$seurat_clusters==char_cl & colnames(vale_clean) %in% rna_cells & colnames(vale_clean) %in% eD_cells)/sum(vale_clean$seurat_clusters==char_cl),1)
   ))
   venn_df <- cbind(venn_df, temp_df)
 }
-colnames(venn_df) <- as.character(c(0:(max(as.integer(vale_clean$sub.cluster))-1) ))
+colnames(venn_df) <- as.character(c(0:(max(as.integer(vale_clean$seurat_clusters))-1) ))
 rownames(venn_df) <- c("total cells",
                        "# of eD cells",
                        "# of rna cells",
@@ -209,12 +236,12 @@ eD_tpr_only <- unname(unlist(venn_df["% of eD cells",])) - unname(unlist(venn_df
 rna_tpr_only <- unname(unlist(venn_df["% of rna cells",])) - unname(unlist(venn_df["% of common cells",]) )
 common <- unname(unlist(venn_df["% of common cells",]))
 
-Values <- matrix(c(rna_tpr_only, common, eD_tpr_only), nrow = 3, ncol = max(as.integer(vale_clean$sub.cluster)), byrow = TRUE)
-barplot(Values, main = "eD vs rna by cluster", names.arg = seq(0, max(as.integer(vale_clean$sub.cluster))-1) , 
+Values <- matrix(c(rna_tpr_only, common, eD_tpr_only), nrow = 3, ncol = max(as.integer(vale_clean$seurat_clusters)), byrow = TRUE)
+barplot(Values, main = "eD vs rna by cluster", names.arg = seq(0, max(as.integer(vale_clean$seurat_clusters))-1) , 
         xlab = "cluster", ylab = "fraction", col = c("grey", "pink", "salmon"))
 #legend(25, 0.1, lwd=3, col=c("salmon", "pink", "grey"), lty=c(1,1,1), legend=c("EmptyDrops_multiome ", "common", "cellRanger-arc") )
-legend("topleft", bg="white", lwd=3, col=c("salmon", "pink", "grey"), lty=c(1,1,1),
-       legend=c("EmptyDrops_multiome ", "common", "cellRanger-arc") )
+legend("topleft", bg="transparent", lwd=3, col=c("salmon", "pink", "grey"), lty=c(1,1,1),
+       legend=c("eD", "common", "eDrna") )
 
 
 
